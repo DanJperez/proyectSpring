a5b346da9951ebbda6679e7c25cca637a2292985
danjperez <splintx5@gmail.com>
2024-11-22 14:54:16 -0600

    archivo

--- src/test/java/com/example/proyecto/ProyectoApplicationTests.java
+++ src/test/java/com/example/proyecto/ProyectoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.PLN;
+package com.example.proyecto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlnApplicationTests {
+class ProyectoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
